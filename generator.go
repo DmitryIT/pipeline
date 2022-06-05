d5a4a26801e9838218cbc85360cb0f44357f603a
dmitry <dmitry>
2022-06-05 12:20:19 +0200

    implement processor and generator

--- generator.go
+++ generator.go
@@ -2,14 +2,13 @@ package main
 
 import "github.com/DmitryIT/pipeline/figures"
 
-func GenerateFigure(figureType figures.FigureType) figures.Figure {
+func GenerateFigure(figureType figures.FigureType, figureChan chan<- figures.Figure) {
 	switch figureType {
 	case figures.TriangleType:
-		return figures.NewTriangle()
+		figureChan <- figures.NewTriangle()
 	case figures.RectangleType:
-		return figures.NewRectangle()
+		figureChan <- figures.NewRectangle()
 	case figures.CircleType:
-		return figures.NewCircle()
+		figureChan <- figures.NewCircle()
 	}
-	return nil
 }
--- processor.go
+++ processor.go
@@ -1,1 +1,19 @@
 package main
+
+import "github.com/DmitryIT/pipeline/figures"
+
+type ProcessingType string
+
+const (
+	Area      ProcessingType = "area"
+	Perimeter ProcessingType = "perimeter"
+)
+
+func ProcessFigure(processType ProcessingType, figure <-chan figures.Figure, resultChan chan<- float64) {
+	switch processType {
+	case Area:
+		resultChan <- (<-figure).Area()
+	case Perimeter:
+		resultChan <- (<-figure).Perimeter()
+	}
+}
