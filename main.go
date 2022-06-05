87a01a217fd0197a3451a4caf48db71f8002341e
dmitry <dmitry>
2022-06-05 16:06:29 +0200

    add Prometheus

--- main.go
+++ main.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"math/rand"
-	"time"
+	"net/http"
 
-	"github.com/DmitryIT/pipeline/figures"
-)
-
-const (
-	NUM_OF_GENERATORS = 2
-	NUM_OF_PROCESSORS = 0
+	"github.com/DmitryIT/pipeline/pipeline"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	figureChan := make(chan figures.Figure)
-	resultChan := make(chan float64)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
-	defer cancel()
-	// start up generators
-	for i := 0; i < NUM_OF_GENERATORS; i++ {
-		// choose random figure type
-		go GenerateFigure(ctx, figures.RandomFugureType(), figureChan)
-	}
-	// start up processors
-	for i := 0; i < NUM_OF_PROCESSORS; i++ {
-		go ProcessFigure(ctx, figures.RandomProcessingType(), figureChan, resultChan)
-	}
+	pipeline.StartPipeline()
 
-	// read from resultChan and print results
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				close(resultChan)
-				return
-			default:
-				fmt.Printf("Result: %f\n", <-resultChan)
-			}
-		}
-	}()
-	<-ctx.Done()
-}
-
-// wait random time from 0 to 5 seconds
-func Delay() {
-	rand.Seed(time.Now().UnixNano())
-	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+	fmt.Printf("Starting server on port 8080\n")
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
 }
---
+++ pipeline/pipeline.go
@@ -0,0 +1,51 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/DmitryIT/pipeline/figures"
+)
+
+const (
+	NUM_OF_GENERATORS = 2
+	NUM_OF_PROCESSORS = 1
+)
+
+func StartPipeline() {
+
+	figureChan := make(chan figures.Figure)
+	resultChan := make(chan float64)
+	ctx := context.Background()
+
+	// start up generators
+	for i := 0; i < NUM_OF_GENERATORS; i++ {
+		// choose random figure type
+		go GenerateFigure(ctx, figures.RandomFugureType(), figureChan)
+	}
+	// start up processors
+	for i := 0; i < NUM_OF_PROCESSORS; i++ {
+		go ProcessFigure(ctx, figures.RandomProcessingType(), figureChan, resultChan)
+	}
+
+	// read from resultChan and print results
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(resultChan)
+				return
+			default:
+				fmt.Printf("Result: %f\n", <-resultChan)
+			}
+		}
+	}()
+}
+
+// wait random time from 0 to 5 seconds
+func Delay() {
+	rand.Seed(time.Now().UnixNano())
+	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+}
